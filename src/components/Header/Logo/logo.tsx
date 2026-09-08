79e94abdc2a558e02ef6f49678536687c1b11392
Kostya <kot161007@gmail.com>
2026-09-09 03:03:30 +0700

    chenging file system

---
+++ src/components/Header/Logo/logo.tsx
@@ -0,0 +1,11 @@
+import style from "./logo.module.css";
+
+export const Logo = () => {
+  return (
+    <span className={style.logo} aria-label="Logo">
+      dawgSkins
+    </span>
+  );
+};
+
+export default Logo;
---
+++ src/type/skins.ts
@@ -0,0 +1,8 @@
+export interface ISkinsData {
+  id: string;
+  name: string;
+  img: string;
+  wear: string;
+  price: number;
+  prieceChange: number;
+}
